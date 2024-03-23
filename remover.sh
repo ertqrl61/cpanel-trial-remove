1963f3b191c325ae17c6e2d80e746000d49a00c3
ertqrl61 <43776613+ertqrl61@users.noreply.github.com>
2024-03-23 19:48:21 +0300

    Update remover.sh

--- remover.sh
+++ remover.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo "Removing cPanel Trial Banners....";
 $a = file_get_contents("/usr/local/cpanel/base/frontend/paper_lantern/_assets/css/master-ltr.cmb.min.css");
 if(strpos($a, "#trialWarningBlock{display:none;}") !== false){
