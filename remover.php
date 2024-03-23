b6c903f798b43022c0b073a3307fa3ff1433e4c3
ertqrl61 <43776613+ertqrl61@users.noreply.github.com>
2024-03-23 20:05:10 +0300

    Update remover.php

--- remover.php
+++ remover.php
@@ -1,12 +1,12 @@
 #!/usr/bin/php
 <?php
 echo "Removing cPanel Trial Banners...";
-$css = file_get_contents("/usr/local/cpanel/base/frontend/paper_lantern/_assets/css/master-ltr.cmb.min.css");
+$css = file_get_contents("/usr/local/cpanel/base/frontend/jupiter/_assets/css/master-ltr.cmb.min.css");
 if (strpos($css, "#trialWarningBlock{display:none;}") !== false) 
 {
     echo "cPanel trial banner already removed!";
 } else {
-    file_put_contents("/usr/local/cpanel/base/frontend/paper_lantern/_assets/css/master-ltr.cmb.min.css",$css . "#trialWarningBlock{display:none;}");
+    file_put_contents("/usr/local/cpanel/base/frontend/jupiter/_assets/css/master-ltr.cmb.min.css",$css . "#trialWarningBlock{display:none;}");
     $ltr = file_get_contents("/usr/local/cpanel/whostmgr/docroot/styles/master-ltr.cmb.min.css");
     if (strpos($ltr, "#divTrialLicenseWarning{display:none}") == false) {
         file_put_contents("/usr/local/cpanel/whostmgr/docroot/styles/master-ltr.cmb.min.css",$ltr . "#divTrialLicenseWarning{display:none}");
